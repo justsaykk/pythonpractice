196f81cb225c6dbc4d6718cbabe7e31f072cadf3
justsaykk <kaikein.woo@gmail.com>
2022-04-28 21:50:53 +0800

    practicing python

---
+++ simple.js
@@ -0,0 +1,21 @@
+// const a = 10;
+// const b = 20;
+// c = 10;
+
+// console.log(a + b + "total:" + a + b);
+
+let a = 20;
+
+for (a; a < 30; a++) {
+  console.log(a);
+}
+
+if ((a = 20)) {
+  console.log("True");
+}
+
+const names = [1, 2, 3, 4, 5];
+
+// names[10] = 15;
+names.push(20);
+console.log(names);
---
+++ simple.py
@@ -0,0 +1,28 @@
+# Data
+# no such thing as "const", "let" and "var". Its all "let".
+a = 10
+b = 20
+c = 50
+
+# Booleans are capitalized
+a_truth = True
+a_lie = False
+
+# Data Storage
+# Arrays (called "lists")
+empty = []
+names = ["kk0", "kk1", "kk2"]
+print(names[1])
+print("length of names array is " + str(len(names)))
+names.append("krystal")
+print("length of new names array is " + str(len(names)))
+
+
+# Conditionals
+# Colons indicate the start of a code block, indentation indicates the end of a code block.
+if c <= 50 and b == True or c == 100:  # booleans are capitalized "True"/"False"
+    print("testing")
+elif c == 30:  # No such thing as "===" in python
+    print("testing 2")
+else:
+    print("going to sleep")
